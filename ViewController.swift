15fbff2f802da5638b9f1340254016616716adc5
Karina Grk <vanillefrancaise133@gmail.com>
2019-03-15 20:45:26 -0400

    Revert "Final gui changes"
    This reverts commit ead7e6ffaa71e915712a69783d51cc06243900be.

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskTracker
+//
+//  Created by Margo on 2019-03-14.
+//  Copyright © 2019 Gbc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
